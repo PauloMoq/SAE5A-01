a71851e8868caf217360f0a9b68baf4332d07e04
zyker465 <42122241+zyker465@users.noreply.github.com>
2023-09-15 16:43:55 +0200

    Ajout page mot de passe oublié

---
+++ src/views/PasswordPage.vue
@@ -0,0 +1,36 @@
+<template>
+   <div class="container">
+    <input type="checkbox" id="check">
+    <div class="Password_Form">
+      <header>Mot de passe oublié</header>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      firstname: '',
+      lastname: '',
+      email: '',
+      password: '',
+      confirmPassword: '', // Champ pour la confirmation du mot de passe
+    };
+  },
+  methods: {
+    register() {
+      // Ajoutez ici la logique d'inscription, y compris la validation du mot de passe et de la confirmation.
+      if (this.password !== this.confirmPassword) {
+        alert("Le mot de passe et la confirmation du mot de passe ne correspondent pas.");
+        return;
+      }
+      // Le reste de la logique d'inscription...
+    },
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
