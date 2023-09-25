32f63ed1bd8644d1b82e252cf45b1ea088fd4748
Théo Massias <massias.theo59@gmail.com>
2023-09-25 09:32:53 +0200

    Rajout de SweetAlert2

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import VueSweetalert2 from 'vue-sweetalert2';
+import 'bootstrap/dist/css/bootstrap.css'
+import 'sweetalert2/dist/sweetalert2.min.css';
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App);
+
+app.use(VueSweetalert2);
+
+app.use(router).mount('#app')
--- src/views/RegisterPage.vue
+++ src/views/RegisterPage.vue
@@ -16,7 +16,7 @@
           <img src="../assets/icon_cadena.png" alt="Icône mdp">
           <input type="password" placeholder="Confirmez votre mot de passe">
         </div>
-        <input type="button" class="button" value="S'inscrire">
+        <input type="button" class="button" value="S'inscrire" @click="register()">
       </form>
       <div class="signup">
         <span class="signup">Tu as déjà un compte?
@@ -41,10 +41,13 @@ export default {
   methods: {
     register() {
       // Ajoutez ici la logique d'inscription, y compris la validation du mot de passe et de la confirmation.
-      if (this.password !== this.confirmPassword) {
-        alert("Le mot de passe et la confirmation du mot de passe ne correspondent pas.");
-        return;
-      }
+      /* if (this.password != this.confirmPassword) {
+        
+      } */
+      this.$swal({
+        text: "Vous n'avez pas mis le même mot de passe dans la boîte de confirmation.",
+        icon: 'error'
+      });
       // Le reste de la logique d'inscription...
     },
   },
