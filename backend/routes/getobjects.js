c2940da75fe3640e4c853b57984f7ba7d8303520
Théo Massias <massias.theo59@gmail.com>
2023-11-27 11:15:47 +0100

    [SAE5A-01 #48]
    Modification du backend pour connexion BDD avec notamment une modification dans getobjects.js, et les packages.

--- backend/routes/getobjects.js
+++ backend/routes/getobjects.js
@@ -1,9 +1,33 @@
 var express = require('express');
 const axios = require('axios');
+const {MongoClient} = require('mongodb');
+const mongoose = require('mongoose');
 var router = express.Router();
+
 
 /* Un moyen de récupérer les oeuvres recherchés */
 router.get('/', async function (req, res, next) {
+  const mongoUri = process.env.MONGO_URI || 'mongodb://mongodb-container:27017/';
+  
+  async function main() {
+    const client = new MongoClient(mongoUri, { useUnifiedTopology: true, useNewUrlParser: true  });
+  
+    try {
+      await client.connect();
+      
+      const mush = client.db("Mush");
+  
+      const result = await mush.listCollections().toArray();
+  
+      console.log(result);
+  
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  
+  main().catch(console.error);
+
   // On récupère les paramètres de la recherche.
   const filtreRecherche = {
     "date_debut": "",
