d19954cc69425300c41d7dfa45e16a5185a478a8
Akvir03 <nathan.devienne.pro@gmail.com>
2023-11-28 17:51:08 +0100

    [SAE5A-01 #36] Modification de la confi vue

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
+  publicPath: process.env.NODE_ENV === 'development' ? '/' : '/',
   devServer: {
-    disableHostCheck: process.env.NODE_ENV === 'development',
+    public: 'traefik.http.services.mushroompy.loadbalancer.server.port=8080',
   },
 };
